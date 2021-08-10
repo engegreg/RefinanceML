115b3f0b9c56fb09f7900d1db6cd8a023f5b3004
Greg Engelmann <44015442+engegreg@users.noreply.github.com>
2021-08-09 23:48:24 -0600

    Update webapp.py
    Removed the data cleaning process from the main application, reformatted to pep8.

--- webapp.py
+++ webapp.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import pandas as pd
 import streamlit as st
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, classification_report, roc_auc_score
+from sklearn.metrics import accuracy_score, precision_score, \
+    recall_score, classification_report, roc_auc_score
 
+
+df=pd.read_csv('loan_data_cleaned.csv')
 
 st.title("Refinance Classification Model")
 
@@ -12,46 +17,8 @@ This is a Machine Learning model that determines whether someone is eligible for
 Adjust the slider to the left to observer the model's behavior with additional depth. 
 """)
 
+#Create interactive slider
 get_n_estimators = st.sidebar.slider("Select depth", 1,25)
-
-
-dfimb = pd.read_csv('loan_data.csv')
-dfimb.drop(dfimb.index[dfimb['purpose'] == 'credit_card'], inplace=True)
-    #create target variable#
-
-dfimb['refi_possible'] = 1
-dfimb.loc[dfimb['fico'] <= 650, 'refi_possible'] = 0
-dfimb.loc[dfimb['inq.last.6mths'] > 1, 'refi_possible'] = 0
-dfimb.loc[dfimb['delinq.2yrs'] > 0, 'refi_possible'] = 0
-    #drop vars used to create target variable#
-
-
-count_class0, count_class1 = dfimb['credit.policy'].value_counts()
-
-    # Creating 2 separate dataframes that contain the different values for credit policy. We'll use this to undersample the data.#
-
-df_class0 = dfimb[dfimb['credit.policy'] == 0]
-df_class1 = dfimb[dfimb['credit.policy'] == 1]
-print(df_class0.shape)
-df_class1.shape
-
-    #Using sample function to create undersampling base. This randomizes the dataset and avoids bias.
-
-df_under_base = df_class1.sample(count_class1)
-
-    #concatenate results.
-
-df = pd.concat([df_under_base, df_class0], axis=0)
-
-mask = {'debt_consolidation': 0, 'all_other': 1, 'home_improvement': 2, 'small_business': 3, 'major_purchase': 4,
-            'educational': 5}
-
-df["Loan_type"] = df["purpose"].replace(mask)
-df.drop(columns='purpose', inplace=True)
-df.drop(columns=['fico', 'inq.last.6mths'], inplace=True)
-
-df.reset_index(drop=True, inplace=True)
-
 
 
 #Creating variables
@@ -62,11 +29,13 @@ X=df.drop(columns=target)
 
 #TTS
 
-X_train, X_val, y_train, y_val = train_test_split(X,y,test_size=0.2, random_state = 1)
+X_train, X_val, y_train, y_val = train_test_split(X,y,
+        test_size=0.2, random_state = 1)
 
 
 st.write(df.head())
 #This function trains the model, returns the metrics of said model, and directly connects to the slider on the web app to make it interactive.
+
 def adjust_depth(slider):
     params = dict()
 
@@ -87,4 +56,3 @@ def adjust_depth(slider):
 
 
 adjust_depth(get_n_estimators)
-
